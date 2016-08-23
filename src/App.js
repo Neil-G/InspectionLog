4ccecc2e7a7b87b33444fdbd1281338f3284d83f
neil <ngneilgordon@gmail.com>
2016-08-23 03:30:51 -0400

    resolve conflict merge in App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,12 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Header } from './components/parts/Header'
-// require("./../stylesheet.css")
-
-
-
-
-=======
-import {Header} from './Header';
-import {CreateOrEditForm} from './CreateOrEditForm';
-
-const section = {
-    borderLeft: "0.5px solid gray"
-  , borderRight: "0.5px solid gray"
-  ,padding: '8px'
-}
->>>>>>> 615cdb6d5aeb34b8eeab99ad14baad472dd4b95f
 
 export class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <div className="container">
         <Header />
         { this.props.children }
-=======
-      <div style={{ overflowX: 'scroll' }} >
-        <Header/>
-
-        <table style={{ position: 'absolute', top: '80px'}}>
-          <thead>
-            <tr>
-              <td rowSpan="2" style={section}>Address</td>
-              <td rowSpan="2" style={section}>Client Status</td>
-              <td colSpan="3">Records on File</td>
-              <td rowSpan="2" style={section}>NYC DOB APP#</td>
-              <td colSpan="2" style={section}>Special Inspection Type</td>
-              <td colSpan="2" style={section}>Copies of Initial Reports On File Date</td>
-              <td rowSpan="2" style={section}>Date of Inspection</td>
-              <td rowSpan="2" style={section}>Inspector Name</td>
-              <td colSpan="2" style={section}>Type of Inspection</td>
-              <td rowSpan="2" style={section}>Item Inspected</td>
-              <td rowSpan="2" style={section}>Inspector Inspection Report on File</td>
-              <td rowSpan="2" style={section}>Inspection Results</td>
-              <td colSpan="2" style={section}>Copies of Final Reports on File</td>
-              <td rowSpan="2" style={section}>Job Signed off Date</td>
-            </tr>
-            <tr>
-              <td>Proposal</td>
-              <td>Engagement Letter</td>
-              <td>Invoice</td>
-              <td>TR1</td>
-              <td>TR8</td>
-              <td>TR1</td>
-              <td>TR8</td>
-              <td>Progress</td>
-              <td>Final</td>
-              <td>TR1</td>
-              <td>TR8</td>
-            </tr>
-          </thead>
-        </table>
->>>>>>> 615cdb6d5aeb34b8eeab99ad14baad472dd4b95f
       </div>
     );
   }
