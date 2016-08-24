96dd8767d8aa4e1ad979e05a5fe4c92f3e712a01
Warren Neil Gordon <Neil@Warrens-Mac-mini.local>
2016-08-24 15:30:49 -0400

    add store and routing reducer; simplify App component format

--- client-src/App.js
+++ client-src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 require('./../public/custom.scss')
 
-export class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Hello</h1>
-      </div>
-    );
-  }
-}
+const App = ({ children }) => (
+  <div className="container">
+    <h1>Hello</h1>
+  </div>
+)
+
+export default App
