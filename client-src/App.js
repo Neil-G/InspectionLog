d628d6384252bf37f8c3951cf3102082d1ed1ee6
neil <ngneilgordon@gmail.com>
2016-08-25 09:54:44 -0400

    move custom.scss to client-src

--- client-src/App.js
+++ client-src/App.js
@@ -1,12 +1,12 @@
 // @flow
 
 import React, { Component } from 'react';
-require('./../public/custom.scss')
+require('./custom.scss')
 
 class App extends Component {
   render() {
     return (
-      <div className="test container">
+      <div className="test-2 container">
         <h1>Hello!</h1>
       </div>
     );
